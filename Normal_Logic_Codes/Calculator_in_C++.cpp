bbd434aa20ca90a24720beddff7be15374e1292d
AtharvaKailasKadam <146631158+AtharvaKailasKadam@users.noreply.github.com>
2024-09-09 06:12:29 +0000

    Simple Calculator Code

---
+++ Normal_Logic_Codes/Calculator_in_C++.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+
+using namespace std;
+
+class Calculator
+{
+	public:
+	double add(double a, double b)
+	{
+		return a+b;
+	}
+	double sub(double a,double b)
+	{
+		return a-b;
+	}
+	double mul(double a,double b)
+	{
+		return a*b;
+	}
+	double div(double a, double b)
+	{
+		return a/b;
+	}
+};
+
+	
+int main()
+{
+	Calculator calc;
+	
+	double num1,num2;
+	
+	cout<<"Enter the First Number: "<<endl;
+	cin>>num1;
+	cout<<"Enter the Second Number: "<<endl;
+	cin>>num2;
+	
+	char op;
+	
+	cout<<"Enter the Operation to be Performed '+','-','*','/'";
+	
+	switch(op)
+	{
+		case'+':
+		cout<<"Result : "<<calc.add(num1,num2)<<endl;
+		break;
+		
+		case'-':
+		cout<<"Result : "<<calc.sub(num1,num2)<<endl;
+		break;
+		
+		case'*':
+		cout<<"Result : "<<calc.mul(num1,num2)<<endl;
+		break;
+		
+		case'/':
+		cout<<"Result : "<<calc.div(num1,num2)<<endl;
+		break;
+		
+		default:
+		cout<<"Invalid Operation";
+	}
+	return 0;
+}
