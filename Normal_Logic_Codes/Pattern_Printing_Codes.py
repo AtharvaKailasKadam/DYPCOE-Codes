f6606270d2642edabbbf0c86c2bccc7860cc7b36
Atharva Kailas Kadam <atharvakailaskadam1809@gmail.com>
2024-09-12 00:32:16 +0530

    Done

---
+++ Normal_Logic_Codes/Pattern_Printing_Codes.py
@@ -0,0 +1,20 @@
+# Code to Print the Pattern as..
+# 1a2bb3ccc and so on..
+
+
+n=int(input("Enter the Number for a : "))
+n1=int(input("Enter the Number for b : "))
+n2=int(input("Enter the Number for c : "))
+
+for i in range(n):
+    print(n)
+    print("a")
+
+print(n1)
+for i in range(n1):
+    print("b")
+
+print(n2)
+for i in range(n2):
+    print("C")
+
